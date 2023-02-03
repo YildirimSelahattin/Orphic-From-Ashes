4bc68ffa6d8b489e2add1a83a05c6747c9527804
emirYarici <e-yarici@hotmail.com>
2023-02-04 00:36:49 +0300

    photo move

---
+++ Assets/Scripts/PhotoThrower.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoThrower : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Transform firePlace;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
