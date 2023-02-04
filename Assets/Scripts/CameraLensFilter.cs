d7d62976902d44474e0b7d04dd0807a8776b6e1c
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-02-04 19:41:12 +0300

    Dialog Box

---
+++ Assets/Scripts/CameraLensFilter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraLensFilter : MonoBehaviour
+{
+    public float trc = 0f;
+    private Image filter;
+
+    void Start()
+   {
+       filter = gameObject.GetComponent<Image>();
+   }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            trc += 0.1f;
+            filter.color = new Color(0,0,0,trc);
+        }
+    }
+}
